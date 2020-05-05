71a05502ae4469ca14c034399d30c5d87eaedaf0
Dante999 <dante999@users.noreply.github.com>
2020-05-05 23:02:03 +0200

    Remove double read/write from pcf8574
    Thought after reading the datasheet that it is necessary to double
    read/write on the gpio expander chip, but it seems that this isn't
    needed.

--- src/pcf8574.c
+++ src/pcf8574.c
@@ -33,15 +33,12 @@ void pcf8574_set(uint8_t byte)
 {
 	i2c_start_wait(PCF8574A_ADDRESS + I2C_WRITE);
 	i2c_write(byte);
-	i2c_write(byte); // must be written twice
 	i2c_stop();
 }
 
 uint8_t pcf8574_get(void)
 {
 	i2c_start_wait(PCF8574A_ADDRESS + I2C_READ);
-	uint8_t a = i2c_read(1);
-	uint8_t b = i2c_read(0); // must be read twice
 
-	return b;
+	return i2c_read(0);
 }
