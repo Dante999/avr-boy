90e743d26a270448010601f2c2603d9c536623c7
Dante999 <dante999@users.noreply.github.com>
2020-06-04 13:08:31 +0200

    Refactored ssd1306.c and reduce block size to 1

--- src/driver/ssd1306.c
+++ src/driver/ssd1306.c
@@ -40,7 +40,7 @@
 #define LCD_BUFFER_SIZE (SSD1306_LCDWIDTH * SSD1306_LCDHEIGHT / 8)
 
 // the size of the i2c-data packages (default: 8bytes will be send at once)
-#define BLOCK_SIZE 8
+#define BLOCK_SIZE 1
 
 /*******************************************************************************
  * ssd1306 specific i2c commands
@@ -82,61 +82,94 @@
 #define SSD1306_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL 0x29
 #define SSD1306_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL  0x2A
 
+/******************************************************************************
+ * @brief   sends the given command-byte to the display
+ *
+ * @param   cmd     the command-byte to send
+ *
+ * @return  none
+ ******************************************************************************/
+static void ssd1306_writecmd(uint8_t cmd)
+{
+	i2c_start_wait(SSD1306_ADDR + I2C_WRITE);
+	i2c_write(0x00);
+	i2c_write(cmd);
+	i2c_stop();
+}
+
+/******************************************************************************
+ * @brief   sends multiple data-bytes at once
+ *
+ * @param   *data   a pointer to the data-streama
+
+ * @param   size    the size of data-bytes (length)
+ *
+ * @return  none
+******************************************************************************/
+static void ssd1306_writedata(uint8_t *data, uint8_t size)
+{
+	i2c_start_wait(SSD1306_ADDR + I2C_WRITE);
+	i2c_write(0x40);
+
+	for (uint8_t i = 0; i < size; i++) {
+		i2c_write(data[i]);
+	}
+
+	i2c_stop();
+}
+
 /*******************************************************************************
  * @brief   initialize the display
  *
- * @param   nonevoid lcd_init(void)
-{
-	ks0108_init();
-}
+ * @param   none
  *
  * @return  none
  *******************************************************************************/
 void ssd1306_init()
 {
-	ssd1306_command(SSD1306_DISPLAYOFF); // 0xAE
+	ssd1306_writecmd(SSD1306_DISPLAYOFF); // 0xAE
 
-	ssd1306_command(SSD1306_SETDISPLAYCLOCKDIV); // 0xD5
-	ssd1306_command(0x80);                       // the suggested ratio 0x80
+	ssd1306_writecmd(SSD1306_SETDISPLAYCLOCKDIV); // 0xD5
+	ssd1306_writecmd(0x80); // the suggested ratio 0x80
 
-	ssd1306_command(SSD1306_SETMULTIPLEX); // 0xA8
+	ssd1306_writecmd(SSD1306_SETMULTIPLEX); // 0xA8
 
-	ssd1306_command(64 - 1);
+	ssd1306_writecmd(64 - 1);
 
-	ssd1306_command(SSD1306_SETDISPLAYOFFSET); // 0xD3
-	ssd1306_command(0x0);                      // no offset
+	ssd1306_writecmd(SSD1306_SETDISPLAYOFFSET); // 0xD3
+	ssd1306_writecmd(0x0);                      // no offset
 
-	ssd1306_command(SSD1306_SETSTARTLINE); // | 0x0);    // line #0
+	ssd1306_writecmd(SSD1306_SETSTARTLINE); // | 0x0);    // line #0
 
-	ssd1306_command(SSD1306_CHARGEPUMP); // 0x8D
-	ssd1306_command(0x14);               // using internal VCC
+	ssd1306_writecmd(SSD1306_CHARGEPUMP); // 0x8D
+	ssd1306_writecmd(0x14);               // using internal VCC
 
-	ssd1306_command(SSD1306_MEMORYMODE); // 0x20
-	ssd1306_command(0x00);               // 0x00 horizontal addressing
+	ssd1306_writecmd(SSD1306_MEMORYMODE); // 0x20
+	ssd1306_writecmd(0x00);               // 0x00 horizontal addressing
 
-	ssd1306_command(SSD1306_SEGREMAP | 0x1); // rotate screen 180
+	ssd1306_writecmd(SSD1306_SEGREMAP | 0x1); // rotate screen 180
 
-	ssd1306_command(SSD1306_COMSCANDEC); // rotate screen 180
+	ssd1306_writecmd(SSD1306_COMSCANDEC); // rotate screen 180
 
-	ssd1306_command(SSD1306_SETCOMPINS); // 0xDA
-	ssd1306_command(0x12);
+	ssd1306_writecmd(SSD1306_SETCOMPINS); // 0xDA
+	ssd1306_writecmd(0x12);
 
-	ssd1306_command(SSD1306_SETCONTRAST); // 0x81
-	ssd1306_command(0xCF);
+	ssd1306_writecmd(SSD1306_SETCONTRAST); // 0x81
+	ssd1306_writecmd(0xCF);
 
-	ssd1306_command(SSD1306_SETPRECHARGE); // 0xd9
-	ssd1306_command(0xF1);
+	ssd1306_writecmd(SSD1306_SETPRECHARGE); // 0xd9
+	ssd1306_writecmd(0xF1);
 
-	ssd1306_command(SSD1306_SETVCOMDETECT); // 0xDB
-	ssd1306_command(0x40);
+	ssd1306_writecmd(SSD1306_SETVCOMDETECT); // 0xDB
+	ssd1306_writecmd(0x40);
 
-	ssd1306_command(SSD1306_DISPLAYALLON_RESUME); // 0xA4
+	ssd1306_writecmd(SSD1306_DISPLAYALLON_RESUME); // 0xA4
 
-	ssd1306_command(SSD1306_NORMALDISPLAY); // 0xA6
+	ssd1306_writecmd(SSD1306_NORMALDISPLAY); // 0xA6
 
-	ssd1306_command(SSD1306_DEACTIVATE_SCROLL);
+	ssd1306_writecmd(SSD1306_DEACTIVATE_SCROLL);
 
-	ssd1306_command(SSD1306_DISPLAYON); // switch on OLED
+	ssd1306_writecmd(SSD1306_DISPLAYON); // switch on OLED
 
 	ssd1306_fill(0x00); // clear the whole display
 }
@@ -153,94 +186,17 @@ void ssd1306_fill(uint8_t symbol)
 	unsigned char buffer[BLOCK_SIZE];
 	memset(buffer, symbol, sizeof(buffer));
 
-	ssd1306_command(SSD1306_COLUMNADDR);
-	ssd1306_command(0);
-	ssd1306_command(SSD1306_LCDWIDTH - 1);
+	ssd1306_writecmd(SSD1306_COLUMNADDR);
+	ssd1306_writecmd(0);
+	ssd1306_writecmd(SSD1306_LCDWIDTH - 1);
 
-	ssd1306_command(SSD1306_PAGEADDR);
-	ssd1306_command(0);
-	ssd1306_command(7);
+	ssd1306_writecmd(SSD1306_PAGEADDR);
+	ssd1306_writecmd(0);
+	ssd1306_writecmd(7);
 
 	for (uint16_t i = 0; i <= LCD_BUFFER_SIZE; i = i + BLOCK_SIZE) {
-		ssd1306_data_block(buffer, BLOCK_SIZE);
+		ssd1306_writedata(buffer, BLOCK_SIZE);
 	}
-}
-
-/******************************************************************************
- * @brief   sends the given command-byte to the display
- *
- * @param   cmd     the command-byte to send
- *
- * @return  none
- ******************************************************************************/
-void ssd1306_command(uint8_t cmd)
-{
-	//	static unsigned char buffer[3];
-
-	//	buffer[0] = SSD1306_ADDR;
-	//	buffer[1] = 0x00;
-	//	buffer[2] = cmd;
-
-	i2c_start_wait(SSD1306_ADDR + I2C_WRITE);
-	i2c_write(0x00);
-	i2c_write(cmd);
-	i2c_stop();
-}
-
-/******************************************************************************
- * @brief   sends the given  data-byte to the display
- *
- * @param   data    the data-byte to send
- *
- * @return  none
- ******************************************************************************/
-void ssd1306_data(uint8_t data)
-{
-	//	static unsigned char buffer[3];
-
-	//	buffer[0] = SSD1306_ADDR;
-	//	buffer[1] = 0x40;
-	//	buffer[2] = data;
-
-	i2c_start_wait(SSD1306_ADDR + I2C_WRITE);
-	i2c_write(0x40);
-	i2c_write(data);
-	i2c_stop();
-}
-
-/******************************************************************************
- * @brief   sends multiple data-bytes at once
- *
- * @param   *data   a pointer to the data-streama
-
- * @param   size    the size of data-bytes (length)
- *
- * @return  none
-******************************************************************************/
-void ssd1306_data_block(uint8_t *data, uint8_t size)
-{
-
-	//	size_t buffer_size = size + 2;
-
-	//	uint8_t buffer[buffer_size];
-
-	//	buffer[0] = SSD1306_ADDR;
-	//	buffer[1] = 0x40;
-
-	//	uint8_t *start = (uint8_t *)&buffer[2];
-
-	//	memcpy(start, data, size);
-
-	//	USI_TWI_Start_Transceiver_With_Data(buffer, buffer_size);
-
-	i2c_start_wait(SSD1306_ADDR + I2C_WRITE);
-	i2c_write(0x40);
-
-	for (uint8_t i = 0; i < size; i++) {
-		i2c_write(data[i]);
-	}
-
-	i2c_stop();
 }
 
 /******************************************************************************
@@ -258,15 +214,15 @@ void ssd1306_data_block(uint8_t *data, uint8_t size)
 void ssd1306_draw_tile_colpage(uint8_t column, uint8_t page, uint8_t *tile)
 {
 
-	ssd1306_command(SSD1306_COLUMNADDR);
-	ssd1306_command(column);
-	ssd1306_command(column + 7);
+	ssd1306_writecmd(SSD1306_COLUMNADDR);
+	ssd1306_writecmd(column);
+	ssd1306_writecmd(column + 7);
 
-	ssd1306_command(SSD1306_PAGEADDR);
-	ssd1306_command(page);
-	ssd1306_command(page);
+	ssd1306_writecmd(SSD1306_PAGEADDR);
+	ssd1306_writecmd(page);
+	ssd1306_writecmd(page);
 
-	ssd1306_data_block(tile, 8);
+	ssd1306_writedata(tile, 8);
 }
 
 /******************************************************************************
@@ -317,15 +273,15 @@ void ssd1306_draw_tile_xy(uint8_t x, uint8_t y, uint8_t *tile)
 void ssd1306_draw_buffer(uint8_t *buffer)
 {
 
-	ssd1306_command(SSD1306_COLUMNADDR);
-	ssd1306_command(0);
-	ssd1306_command(SSD1306_LCDWIDTH - 1);
+	ssd1306_writecmd(SSD1306_COLUMNADDR);
+	ssd1306_writecmd(0);
+	ssd1306_writecmd(SSD1306_LCDWIDTH - 1);
 
-	ssd1306_command(SSD1306_PAGEADDR);
-	ssd1306_command(0);
-	ssd1306_command(7);
+	ssd1306_writecmd(SSD1306_PAGEADDR);
+	ssd1306_writecmd(0);
+	ssd1306_writecmd(7);
 
 	for (uint16_t i = 0; i <= LCD_BUFFER_SIZE; i = i + BLOCK_SIZE) {
-		ssd1306_data_block(buffer + i, BLOCK_SIZE);
+		ssd1306_writedata(buffer + i, BLOCK_SIZE);
 	}
 }
--- src/driver/ssd1306.h
+++ src/driver/ssd1306.h
@@ -6,8 +6,8 @@
  * see the source-file for further documentation
  *
  *******************************************************************************/
-#ifndef _SSD1306_H
-#define _SSD1306_H
+#ifndef SSD1306_H
+#define SSD1306_H
 
 #include <stdint.h>
 
@@ -18,8 +18,6 @@ void ssd1306_draw_tile_xy(uint8_t x, uint8_t y, uint8_t *tile);
 void ssd1306_init(void);
 void ssd1306_fill(uint8_t symbol);
 void ssd1306_draw_tile_colpage(uint8_t column, uint8_t page, uint8_t *tile);
-void ssd1306_command(uint8_t cmd);
-void ssd1306_data(uint8_t data);
-void ssd1306_data_block(uint8_t *data, uint8_t size);
+
 void ssd1306_draw_buffer(uint8_t *buffer);
 #endif
