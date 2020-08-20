98e48c2023d28aebcb3f7fd0957afaece7c30888
Dante999 <dante999@users.noreply.github.com>
2020-08-20 22:02:31 +0200

    Improve userport with HIGH,LOW,TOGGLE modes

--- src/userport.c
+++ src/userport.c
@@ -2,24 +2,34 @@
 
 #include <avr/io.h>
 
+static void set_reg(volatile uint8_t *reg, uint8_t bit, uint8_t state)
+{
+	switch (state) {
+	case USERPORT_LOW:
+		*reg &= ~(1 << bit);
+		break;
+
+	case USERPORT_HIGH:
+		*reg |= (1 << bit);
+		break;
+
+	case USERPORT_TOGGLE:
+		*reg ^= (1 << bit);
+	}
+}
+
 void userport_init(void)
 {
 	DDRD |= (1 << PD6);
 	DDRC |= (1 << PC7);
 }
 
-void userport_11(bool high)
+void userport_11(uint8_t stat)
 {
-	if (high)
-		PORTD |= (1 << PD6);
-	else
-		PORTD &= ~(1 << PD6);
+	set_reg(&PORTD, PD6, stat);
 }
 
-void userport_12(bool high)
+void userport_12(uint8_t stat)
 {
-	if (high)
-		PORTC |= (1 << PC7);
-	else
-		PORTC &= ~(1 << PC7);
+	set_reg(&PORTC, PC7, stat);
 }
--- src/userport.h
+++ src/userport.h
@@ -1,10 +1,14 @@
 #ifndef USERPORT_H
 #define USERPORT_H
 
-#include <stdbool.h>
+#include <stdio.h>
+
+#define USERPORT_LOW    0
+#define USERPORT_HIGH   1
+#define USERPORT_TOGGLE 2
 
 void userport_init(void);
-void userport_11(bool high);
-void userport_12(bool high);
+void userport_11(uint8_t stat);
+void userport_12(uint8_t stat);
 
 #endif /* USERPORT_H */
