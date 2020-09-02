a5d7bc8e8dd362b80fa60354b5d4adaaca24a079
Dante999 <dante999@users.noreply.github.com>
2020-09-02 19:11:01 +0200

    Add defines in lcd.c for easier display configuration

--- src/handheld/display/lcd.c
+++ src/handheld/display/lcd.c
@@ -4,61 +4,54 @@
 #include "driver/ssd1306.h"
 #include "graphx.h"
 
-static void lcd_draw_byte(uint8_t x, uint8_t y, uint8_t byte)
-{
-	for (uint8_t j = 0; j < 8; j++) {
-
-		uint8_t color =
-		    (byte & (1 << j)) ? GRAPHX_PIXEL_ON : GRAPHX_PIXEL_OFF;
-
-		lcd_draw_pixel(x, y + j, color);
-	}
-}
+#define LCD_TYPE_KS0108
+//#define LCD_TYPE_SSD1306
 
 void lcd_init(void)
 {
+#ifdef LCD_TYPE_KS0108
 	ks0108_init();
+#endif
+
+#ifdef LCD_TYPE_SSD1306
+	ssd1306_init();
+#endif
 }
 
 void lcd_draw_pixel(uint8_t x, uint8_t y, uint8_t color)
 {
+#ifdef LCD_TYPE_KS0108
 	ks0108_set_pixel(x, y, color);
+#else
+	(void)x;
+	(void)y;
+	(void)color;
+#endif
 }
 
 void lcd_scroll(uint8_t line)
 {
+#ifdef LCD_TYPE_KS0108
 	ks0108_scroll(line);
+#else
+	(void)line;
+#endif
 }
 
 void lcd_drawbuffer(uint8_t *buffer)
 {
+#ifdef LCD_TYPE_KS0108
 	ks0108_drawbuffer(buffer);
-}
+#endif
 
-void lcd_drawbyte(uint8_t page, uint8_t addr, uint8_t byte)
-{
-	ks0108_draw_byte(page, addr, 1, &byte);
-}
-
-void lcd_draw_sprite(uint8_t x, uint8_t y, uint8_t w, uint8_t h,
-                     const uint8_t *data)
-{
-	for (uint8_t row = 0; row < (h / 8); row++) {
-
-		for (uint8_t col = 0; col < w; col++) {
-
-			uint16_t offset = row * w;
-
-			uint8_t d_new = data[offset + col];
-			uint8_t x_new = x + col;
-			uint8_t y_new = y + (row * 8);
-
-			lcd_draw_byte(x_new, y_new, d_new);
-		}
-	}
+#ifdef LCD_TYPE_SSD1306
+	ssd1306_draw_buffer(buffer);
+#endif
 }
 
 void lcd_draw_sprite8x8(uint8_t x, uint8_t y, const uint8_t *data)
 {
-	lcd_draw_sprite(x, y, 8, 8, data);
+	(void)x;
+	(void)y;
+	(void)data;
 }
